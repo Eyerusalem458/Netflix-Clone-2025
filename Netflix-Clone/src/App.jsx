c2cc55d8231c988f9a06fa3cf51226273cb38702
Eyerusalem <jerrykinu2885@gmail.com>
2025-11-29 16:06:41 +0300

    header and footer

--- Netflix-Clone/src/App.jsx
+++ Netflix-Clone/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello it's working</h1>
+      <Home />
     </div>
   );
 }
