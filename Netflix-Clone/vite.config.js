2aeb3e68ca90acbc23c446a75a971efae44548cf
Eyerusalem <jerrykinu2885@gmail.com>
2025-12-01 20:39:24 +0300

    final deployment

--- Netflix-Clone/vite.config.js
+++ Netflix-Clone/vite.config.js
@@ -5,7 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/Netflix-Clone-2025",
   plugins: [react()],
-  build: {
-    outDir: "build", // This is the default, but explicit is good
-  },
+  
 });
